ab3888fbb06a03760fab1dae5e19a783622152e5
sssv587 <1092663295@qq.com>
2022-07-31 23:39:36 +0800

    @CookieValue,CharacterEncodingFilter

--- springMVC-demo2/src/main/java/com/futurebytedance/spring/controller/ParamController.java
+++ springMVC-demo2/src/main/java/com/futurebytedance/spring/controller/ParamController.java
@@ -1,11 +1,14 @@
 package com.futurebytedance.spring.controller;
 
+import com.futurebytedance.spring.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 
 /**
@@ -19,6 +22,7 @@ public class ParamController {
     @RequestMapping("/testServletAPI")
     //形参位置的request表示当前请求
     public String testServletAPI(HttpServletRequest request) {
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username:" + username + " password:" + password);
@@ -32,11 +36,19 @@ public class ParamController {
             String password,
             String[] hobby,
             //与@RequestParam参数一致
-            @RequestHeader(value = "Host", required = true, defaultValue = "localhost:8080") String host) {
+            @RequestHeader(value = "Host", required = true, defaultValue = "localhost:8080") String host,
+            @CookieValue("JSESSIONID") String JSESSIONID) {
         //多请求参数中出现多个同名的请求参数，可以在控制器方法的形参位置设置字符串类或字符串数组接收此参数
         //若使用字符串类型的形参，最终结果为请求参数的每一个值之间使用逗号进行拼接
         System.out.println("username:" + username + " password:" + password + " hobby:" + Arrays.toString(hobby));
         System.out.println("host:" + host);
+        System.out.println("JSESSIONID:" + JSESSIONID);
+        return "success";
+    }
+
+    @RequestMapping("/testBean")
+    public String testBean(User user) {
+        System.out.println(user);
         return "success";
     }
 }
