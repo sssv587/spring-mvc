7611995ed1a884383e460dbf4c8617a3157f407b
sssv587 <1092663295@qq.com>
2022-09-04 18:29:17 +0800

    RESTFul-实现删除功能

--- springMVC-rest/src/main/java/com/futurebytedance/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/futurebytedance/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.futurebytedance.dao.EmployeeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,5 +27,11 @@ public class EmployeeController {
         Collection<Employee> employeeList = employeeDAO.getAll();
         model.addAttribute("employeeList", employeeList);
         return "employee_list";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        employeeDAO.delete(id);
+        return "redirect:/employee";
     }
 }
